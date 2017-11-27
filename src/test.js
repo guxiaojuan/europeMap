d75c0e787ecb4f8f69c9eb276d93420ec69bfa6d
guxiaojuan <guxiaojuan@500wan.com>
2017-11-27 17:23:59 +0800

    'init'

---
+++ src/test.js
@@ -0,0 +1,6 @@
+const mychart = echarts.init(document.getElementById('europe'))
+
+$.getJSON('../Europe/Europe.json',function (europeData) {
+    europeData = europeData.map(function (serieData, idx) {
+    })
+})
