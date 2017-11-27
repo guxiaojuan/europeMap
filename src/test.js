903e0921c55e776109348085361757e2aaeb5fbf
guxiaojuan <1634747911@qq.com>
2017-11-27 22:10:53 +0800

    '数据初始化'

--- src/test.js
+++ src/test.js
@@ -1,15 +1,16 @@
 const mychart = echarts.init(document.getElementById('europe'))
 
-$.getJSON('../Europe/Europe4.json',function (europeData) {
-    echarts.registerMap('europe',europeData)
+var myData = [
+    {name:'海门',value:[121.15,90]},
+    {name:'舟山',value:[109.783, 39.6]}
+]
 
+$.getJSON('../Europe/Europe4.json',function (europeData) {
     mychart.setOption({
         series:[{
             type:'map',
             map:'europe',
-            data:[{
-                name:'itly',
-            }]
+            data:myData
         }]
     })
 })
